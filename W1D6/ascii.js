2babaec93c3c26c9ed80abfe329ba789ff15d39f
Tanvir-personal <tanvirmahboob@gmail.com>
2022-10-10 09:08:42 -0500

    Update ascii.js

--- W1D6/ascii.js
+++ W1D6/ascii.js
@@ -1,4 +1,4 @@
-"use struct";
+"use strict";
 
 const DEFAULT_SPEED = 250;
 const TURBO_SPEED = 50;
@@ -14,7 +14,7 @@ const FONT_SIZE = {
 	'Large': '16pt',
 	'Extra Large': '24pt',
 	'XXL': '32pt',
-}
+};
 
 
 window.onload = () => {
@@ -54,7 +54,7 @@ const startAction = () => {
    speed = goTurbo();
    console.log('speed: ' + speed);
    time = setInterval(animFrames, speed, animArray, length);
-}
+};
 
 
 const stopAction = () => {
@@ -68,19 +68,19 @@ const stopAction = () => {
    document.getElementById("animation").disabled = false;
    document.getElementById("start").disabled = false; 
    document.getElementById("stop").disabled = true; 
-}
+};
 
 const animFrames = (array, length) => {
    const box = document.getElementById("text-area");
    box.value = array[index % length];
    index ++;
-}
+};
 
 
 const changeAnimation = () => {
    const anim = document.getElementById("animation");
    document.getElementById("text-area").value = ANIMATIONS[anim.value];
-}
+};
 
 
 const changeSize = () => {
@@ -89,10 +89,10 @@ const changeSize = () => {
    box.style.fontSize = FONT_SIZE[biggieSmalls.value];
 //    box.className = FONT_SIZE[biggieSmalls.value];
    console.log(box.className);
-}
+};
 
 
 function goTurbo(){
 	const turboCheckbox = document.getElementById('turbo');
    return (turboCheckbox.checked)? TURBO_SPEED : DEFAULT_SPEED;
-}
+};
